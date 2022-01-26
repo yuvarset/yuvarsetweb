bc7b6fee20d9f1732261c7c152502265a2673f1f
danielkokkaravalel <danieldg007@gmail.com>
2022-01-26 13:08:44 +0530

    Added 'Forgot Credentials' Functionality

--- app/main.py
+++ app/main.py
@@ -53,3 +53,32 @@ def user():
     cur.execute(f"SELECT * from users WHERE username = '{login.username}'")
     rows = cur.fetchall()
     return render_template("user.html", rows=rows)
+
+# Forgot Credentials Functionality
+@app.route('/forgot')
+def forgot():
+    return render_template('forgot.html')
+
+@app.route('/mailPass', methods=["POST"])
+def mailPass():
+    con = mysql.connection
+    cur = con.cursor()
+    cur.execute(f"SELECT * from users WHERE email = '{request.form['email']}'")
+    rows = list(cur.fetchall())
+    if len(rows) == 0:
+        return render_template("forgot.html", msg='Email not registered, Click to go back')
+    else:
+        EMAIL_ADDRESS = os.environ.get("EMAIL_ADDRESS")
+        EMAIL_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+
+        msg = EmailMessage()
+        msg['Subject'] = '[YUVA SUPPORT] Login Details for YMMS'
+        msg['From'] = EMAIL_ADDRESS
+        msg['To'] = request.form['email']
+        msg.set_content(f"Hi {rows[0]['name']} \n\nYour login details are as follows :\n     Username : {rows[0]['username']} \n     Password : {rows[0]['password']}\n\nPlease avoid repling to this email \nHave a great day \n\n\nKind Regards, \nTeam YUVA")
+
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+            smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+            smtp.send_message(msg)
+
+        return  render_template("forgot.html",msg = 'Login details sent succesfully. Click to Login')
